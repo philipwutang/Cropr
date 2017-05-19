45bf70cb5fc655bda7041154a4c8ed42be5ca6f3
Philip Tang <philip.tang@uwaterloo.ca>
2017-05-19 16:43:48 -0400

    Week Three Updates

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  var jcrop_api;
+
+  jcrop_api = $.Jcrop('#cropbox');
 
   // fix menu when passed
   $('.masthead').visibility({
@@ -14,5 +17,21 @@ $(document).ready(function() {
   // create sidebar and attach to menu open
   $('.ui.sidebar').sidebar('attach events', '.toc.item');
 
-  $('.ui.accordion').accordion(function(){});
+  $('.ui.accordion').accordion(function() {});
+
+  jcrop_api.setOptions({
+    trackDocument: true,
+    onChange: showCoords,
+    onSelect: showCoords,
+    bgColor: 'black',
+    bgOpacity: .4
+  });
+
+  $('input').on('change', function() {
+    console.log(this.id);
+    jcrop_api.setOptions({
+      aspectRatio: this.id
+    });
+    jcrop_api.focus();
+  });
 });
