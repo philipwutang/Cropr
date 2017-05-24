d09fb734d1d6d4e184157fa23fe1b815b9da5307
Philip Tang <philip.tang@uwaterloo.ca>
2017-05-24 16:57:49 -0400

    Event Handlers 0.2
    Added ability to set custom dimensions

--- script/script.js
+++ script/script.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
-  var jcrop_api;
+  var jcrop_api, imageWidth, imageHeight;
 
   jcrop_api = $.Jcrop('#cropbox');
-
-  function setImageSize() {
-    console.log("setImageSize");
-    jcrop_api.setOptions({
-      maxSize: [width.value, height.value],
-      minSize: [width.value, height.value]
-    });
-  }
 
   // fix menu when passed
   $('.masthead').visibility({
@@ -36,43 +28,85 @@ $(document).ready(function() {
   });
 
   function showCoords(c) {
-    $('#width').val(Math.round(c.w));
-    $('#height').val(Math.round(c.h));
+    imageWidth = Math.round(c.w);
+    imageHeight = Math.round(c.h);
+    $('#width').val(imageWidth);
+    $('#height').val(imageHeight);
   };
 
-  $('input').on('change', function() {
-    console.log(this.id);
+  function disableRatios() {
+    document.getElementById("1:1").checked = false;
+    document.getElementById("3:2").checked = false;
+    document.getElementById("4:3").checked = false;
+    document.getElementById("16:9").checked = false;
+    document.getElementById("square").classList.add("disabled");
+    document.getElementById("3by2").classList.add("disabled");
+    document.getElementById("4by3").classList.add("disabled");
+    document.getElementById("hd").classList.add("disabled");
+  }
 
+  function enableRatios() {
+    document.getElementById("square").classList.remove("disabled");
+    document.getElementById("3by2").classList.remove("disabled");
+    document.getElementById("4by3").classList.remove("disabled");
+    document.getElementById("hd").classList.remove("disabled");
+  }
+
+  document.getElementById("freeform").checked = true;
+
+  $('input[name=freeform]').change(function() {
+    if ($(this).is(':checked')) {
+      console.log("freeform");
+      disableRatios();
+      jcrop_api.setOptions({
+        aspectRatio: 0
+      });
+    } else {
+      enableRatios();
+      document.getElementById("1:1").checked = true;
+      jcrop_api.setOptions({
+        aspectRatio: 1
+      });
+    }
+  });
+
+  $('input').on('change', function() {
     switch (this.id) {
       case "1:1":
+        document.getElementById("freeform").checked = false;
         jcrop_api.setOptions({
           aspectRatio: 1
         });
         break;
       case "3:2":
+        document.getElementById("freeform").checked = false;
         jcrop_api.setOptions({
           aspectRatio: 3 / 2
         });
         break;
       case "4:3":
+        document.getElementById("freeform").checked = false;
         jcrop_api.setOptions({
           aspectRatio: 4 / 3
         });
         break;
       case "16:9":
+        document.getElementById("freeform").checked = false;
         jcrop_api.setOptions({
           aspectRatio: 16 / 9
         });
         break;
       default:
-        console.log("here");
-        jcrop_api.setOptions({
-          maxSize: [0, 0],
-          minSize: [0, 0],
-          aspectRatio: 0
-        });
+        console.log("nothing");
         break;
     }
     jcrop_api.focus();
+  });
+
+  document.getElementById("setImageSize").addEventListener("click", function() {
+    console.log("setImageSize");
+    var width = document.getElementById('width').value
+    var height = document.getElementById('height').value
+    jcrop_api.animateTo([0, 0, width, height]);
   });
 });
