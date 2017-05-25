3920ec88bed0dc38085c15a03f68bea1cbfa4ff9
Philip Tang <philip.tang@uwaterloo.ca>
2017-05-25 17:18:04 -0400

    Week Four Update

--- script/script.js
+++ script/script.js
@@ -35,35 +35,40 @@ $(document).ready(function() {
   };
 
   function disableRatios() {
-    document.getElementById("1:1").checked = false;
-    document.getElementById("3:2").checked = false;
-    document.getElementById("4:3").checked = false;
-    document.getElementById("16:9").checked = false;
-    document.getElementById("square").classList.add("disabled");
-    document.getElementById("3by2").classList.add("disabled");
-    document.getElementById("4by3").classList.add("disabled");
-    document.getElementById("hd").classList.add("disabled");
+    document.getElementById('1:1').checked = false;
+    document.getElementById('3:2').checked = false;
+    document.getElementById('4:3').checked = false;
+    document.getElementById('16:9').checked = false;
+    document.getElementById('square').classList.add("disabled");
+    document.getElementById('3by2').classList.add("disabled");
+    document.getElementById('4by3').classList.add("disabled");
+    document.getElementById('hd').classList.add("disabled");
   }
 
   function enableRatios() {
-    document.getElementById("square").classList.remove("disabled");
-    document.getElementById("3by2").classList.remove("disabled");
-    document.getElementById("4by3").classList.remove("disabled");
-    document.getElementById("hd").classList.remove("disabled");
+    document.getElementById('square').classList.remove("disabled");
+    document.getElementById('3by2').classList.remove("disabled");
+    document.getElementById('4by3').classList.remove("disabled");
+    document.getElementById('hd').classList.remove("disabled");
   }
 
-  document.getElementById("freeform").checked = true;
+  function freeFormOn() {
+    console.log("freeform");
+    disableRatios();
+    jcrop_api.setOptions({
+      aspectRatio: 0
+    });
+  }
+
+  function freeFormOff() {
+    document.getElementById('freeform').checked = false;
+  }
 
   $('input[name=freeform]').change(function() {
     if ($(this).is(':checked')) {
-      console.log("freeform");
-      disableRatios();
-      jcrop_api.setOptions({
-        aspectRatio: 0
-      });
+      freeFormOn();
     } else {
       enableRatios();
-      document.getElementById("1:1").checked = true;
       jcrop_api.setOptions({
         aspectRatio: 1
       });
@@ -73,40 +78,47 @@ $(document).ready(function() {
   $('input').on('change', function() {
     switch (this.id) {
       case "1:1":
-        document.getElementById("freeform").checked = false;
+        enableRatios();
         jcrop_api.setOptions({
           aspectRatio: 1
         });
         break;
       case "3:2":
-        document.getElementById("freeform").checked = false;
+        enableRatios();
         jcrop_api.setOptions({
           aspectRatio: 3 / 2
         });
         break;
       case "4:3":
-        document.getElementById("freeform").checked = false;
+        enableRatios();
         jcrop_api.setOptions({
           aspectRatio: 4 / 3
         });
         break;
       case "16:9":
-        document.getElementById("freeform").checked = false;
+        enableRatios();
         jcrop_api.setOptions({
           aspectRatio: 16 / 9
         });
         break;
       default:
-        console.log("nothing");
         break;
     }
     jcrop_api.focus();
   });
 
-  document.getElementById("setImageSize").addEventListener("click", function() {
+  $('#setImageSize').on('click', function() {
     console.log("setImageSize");
-    var width = document.getElementById('width').value
-    var height = document.getElementById('height').value
+    var width = $('#width').val();
+    var height = $('#height').val();
     jcrop_api.animateTo([0, 0, width, height]);
+    freeFormOn();
   });
+
+  function init() {
+    $('#freeform').prop('checked', true);
+    disableRatios();
+  }
+
+  init();
 });
